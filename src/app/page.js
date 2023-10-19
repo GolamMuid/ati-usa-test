5830e278c8d2c2d284cedee9bf3a055f62f0b6e6
GolamMuid <golammuid@gmail.com>
2023-10-19 17:53:56 +0600

    added new images

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,180 @@
-import Image from 'next/image'
+"use client";
+
+import React, { useRef, useState, useEffect } from "react";
+import { Swiper, SwiperSlide, useSwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/effect-cards";
+import { Navigation } from "swiper/modules";
+import { Pagination } from "swiper/modules";
+import Image from "next/image";
+import { EffectCards } from "swiper/modules";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [swiper, setSwiper] = useState(null);
+	const [swiper2, setSwiper2] = useState(null);
+	const [swiper3, setSwiper3] = useState(null);
+	const [swiper4, setSwiper4] = useState(null);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const goToPreviousSlide = () => {
+		if (swiper) {
+			swiper.slidePrev();
+			swiper2.slidePrev();
+			swiper3.slidePrev();
+			swiper4.slidePrev();
+		}
+	};
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	const goToNextSlide = () => {
+		if (swiper) {
+			swiper.slideNext();
+			swiper2.slideNext();
+			swiper3.slideNext();
+			swiper4.slideNext();
+		}
+	};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	return (
+		<div className="relative container m-auto overflow-hidden">
+			<div className="relative h-fit overflow-hidden">
+				<Swiper
+					// navigation={true}
+					// modules={[Navigation]}
+					cssMode={true}
+					className="mySwiper"
+					onSwiper={(swiper) => setSwiper(swiper)}
+				>
+					<SwiperSlide>
+						<Image
+							className="first-slider"
+							src="/Screenshot_1.png"
+							width={100}
+							height={100}
+							sizes="100vw"
+						/>
+					</SwiperSlide>
+					<SwiperSlide>
+						<Image
+							className="first-slider"
+							src="/Screenshot_2.png"
+							width={100}
+							height={100}
+							sizes="100vw"
+						/>
+					</SwiperSlide>
+					<SwiperSlide>
+						<Image
+							className="first-slider"
+							src="/Screenshot_3.png"
+							width={100}
+							height={100}
+							sizes="100vw"
+						/>
+					</SwiperSlide>
+				</Swiper>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+				{/* 2nd Slider  */}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+				<div className="absolute z-10 w-40 h-40 overflow-hidden top-[40%] left-24">
+					<Swiper
+						effect={"cards"}
+						grabCursor={true}
+						modules={[EffectCards]}
+						className="mySwiper"
+						onSwiper={(swiper3) => setSwiper3(swiper3)}
+					>
+						<SwiperSlide>
+							<Image src="/ss1.png" width={144} height={144} />
+						</SwiperSlide>
+						<SwiperSlide>
+							<Image src="/ss2.png" width={144} height={144} />
+						</SwiperSlide>
+						<SwiperSlide>
+							<Image src="/ss3.png" width={144} height={144} />
+						</SwiperSlide>
+					</Swiper>
+				</div>
+
+				<div className="absolute z-10 w-[50%] h-36 overflow-hidden top-[50%] right-24 bg-[#00000011]">
+					<Swiper
+						direction={"vertical"}
+						pagination={{
+							clickable: true,
+						}}
+						modules={[Pagination]}
+						className="mySwiper text-white"
+						cssMode={true}
+						onSwiper={(swiper4) => setSwiper4(swiper4)}
+						// className="bg-transparent"
+					>
+						<SwiperSlide className="">
+							<div className="text-base p-2 w-fit text-left">
+								Clark learns about the source of his abilities and his real home
+								when he enters a Kryptonian ship in the Artic. However, an old
+								enemy follows him to Earth in search of a codex and brings
+								destruction.
+							</div>
+						</SwiperSlide>
+						<SwiperSlide className="">
+							<div className="text-base p-2 w-fit text-left">
+								Bruce Wayne and Diana Prince try to bring the metahumans of
+								Earth together after the death of Clark Kent. Meanwhile,
+								Darkseid sends Steppenwolf to Earth with a vast army to
+								subjugate humans.
+							</div>
+						</SwiperSlide>
+						<SwiperSlide className="">
+							<div className="text-base p-2 w-fit text-left">
+								Bruce Wayne, a billionaire, believes that Superman is a threat
+								to humanity after his battle in Metropolis. Thus, he decides to
+								adopt his mantle of Batman and defeat him once and for all.
+							</div>
+						</SwiperSlide>
+					</Swiper>
+				</div>
+
+				<div className="absolute z-10 overflow-hidden top-[40%] text-3xl left-1/2 transform -translate-x-1/2 bg-[#00000011] text-white">
+					DC COMICS
+				</div>
+
+				<div className="absolute z-20 bottom-0 h-16 right-4 w-fit">
+					<Swiper
+						direction={"vertical"}
+						pagination={{
+							clickable: true,
+						}}
+						modules={[Pagination]}
+						className="mySwiper"
+						onSwiper={(swiper2) => setSwiper2(swiper2)}
+						// className="bg-transparent"
+					>
+						<SwiperSlide className="">
+							<div className="text-3xl p-4 w-fit"> MAN OF STEEL </div>
+						</SwiperSlide>
+						<SwiperSlide className="">
+							<div className="text-3xl p-4 w-fit">JUSTICE LEAGUE</div>
+						</SwiperSlide>
+						<SwiperSlide className="">
+							<div className="text-3xl p-4 w-fit"> BATMAN v SUPERMAN </div>
+						</SwiperSlide>
+					</Swiper>
+				</div>
+			</div>
+
+			<div className="mt-4 absolute bottom-0 z-10 bg-white left-[40%] w-full">
+				<button
+					onClick={goToPreviousSlide}
+					className="bg-blue-500 text-white px-4 py-2 m-2"
+				>
+					Prev
+				</button>
+				<button
+					onClick={goToNextSlide}
+					className="bg-blue-500 text-white px-4 py-2 m-2"
+				>
+					Next
+				</button>
+			</div>
+		</div>
+	);
 }
